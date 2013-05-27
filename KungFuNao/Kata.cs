f15a90b3a0fe857d43fce6430dd548fc31a6970c
Bas Bootsma <bootsman@gmail.com>
2013-05-27 11:10:48 +0200

    Initial project.

---
+++ KungFuNao/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Kinect.Toolbox;
+
+namespace KungFuNao
+{ //: IEnumerable<KataTechnique>
+    class Kata
+    {
+        private string name;
+        private IList<KataTechnique> techniques;
+
+        public Kata( string name )
+        {
+            this.name = name;
+            this.techniques = new List<KataTechnique>();
+        }
+
+        public void Add(KataTechnique technique)
+        {
+            this.techniques.Add(technique);
+        }
+
+        public int Size()
+        {
+            return this.techniques.Count;
+        }
+
+        /*
+        public IEnumerator<KataTechnique> GetEnumerator()
+        {
+            return this.techniques.GetEnumerator();
+        }
+         * */
+    }
+}
